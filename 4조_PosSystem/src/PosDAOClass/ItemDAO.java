ea460fcef9f55fd416b0ddb0a1bc6d76b2fd8e2d
GoodLuckDay <haon4658@gmail.com>
2017-12-16 00:22:52 +0900

    Maybe we miscalculated the design.

--- 4조_PosSystem/src/PosDAOClass/ItemDAO.java
+++ 4조_PosSystem/src/PosDAOClass/ItemDAO.java
@@ -1,5 +1,40 @@
 package PosDAOClass;
 
 public class ItemDAO {
+    private int itemNo;
+    private String itemName;
+    private int itemPrice;
+    private int itemStock;
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemStock() {
+        return itemStock;
+    }
+
+    public void setItemStock(int itemStock) {
+        this.itemStock = itemStock;
+    }
+
+    public int getItemNo() {
+        return itemNo;
+    }
+
+    public void setItemNo(int itemNo) {
+        this.itemNo = itemNo;
+    }
 }
--- 4조_PosSystem/src/PosDAOClass/ItemListDAO.java
+++ 4조_PosSystem/src/PosDAOClass/ItemListDAO.java
@@ -1,5 +1,5 @@
 package PosDAOClass;
 
 public class ItemListDAO {
-	
+
 }
