e5d8b520204fd9e58ea49ba6d6c1465dede188fd
GoodLuckDay <haon4658@gmail.com>
2017-12-16 16:24:55 +0900

    ItemList 가져오기 기능 구

--- 4조_PosSystem/src/PosDAOClass/ItemDAO.java
+++ 4조_PosSystem/src/PosDAOClass/ItemDAO.java
@@ -36,45 +36,89 @@ public class ItemDAO {
 
     public static void createItem(String item, int price, int stock) {
         Connection connection = null;
-        PreparedStatement statement = null;
+        PreparedStatement preparedStatement = null;
         try {
             connection = DBConnectionPoolMgr.geteDataSource().getConnection();
 
             StringBuffer query = new StringBuffer();
             StringBuffer cQuery = new StringBuffer();
-            query.append("SELECT IFNULL(MAX('Y'), 'N') AS item_exist_yn FROM item WHERE item_name = '"+item+"'");
+            query.append("SELECT IFNULL(MAX('Y'), 'N') AS item_exist_yn FROM item WHERE item_name = '" + item + "'");
 
-            statement = connection.prepareStatement(query.toString());
+            preparedStatement = connection.prepareStatement(query.toString());
 
-            ResultSet rs = statement.executeQuery(query.toString());
+            ResultSet rs = preparedStatement.executeQuery(query.toString());
             rs.next();
             String itemExistYn = rs.getString("item_exist_yn");
 
-            if(itemExistYn.equals("Y")){
-                return ;
-            }
-            else {
+            if (itemExistYn.equals("Y")) {
+                return;
+            } else {
                 cQuery.append("INSERT INTO item(item_name, item_price, item_stock) values(?,?,?)");
-                statement = connection.prepareStatement(cQuery.toString());
-                statement.setString(1, item);
-                statement.setInt(2, price);
-                statement.setInt(3, stock);
-                statement.executeUpdate();
+                preparedStatement = connection.prepareStatement(cQuery.toString());
+                preparedStatement.setString(1, item);
+                preparedStatement.setInt(2, price);
+                preparedStatement.setInt(3, stock);
+                preparedStatement.executeUpdate();
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (statement != null) {
+            if (preparedStatement != null) {
                 try {
-                    statement.close();
+                    preparedStatement.close();
                 } catch (SQLException e) {
                 }
             }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
 
+    public static void getItemList() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DBConnectionPoolMgr.geteDataSource().getConnection();
+            StringBuffer query = new StringBuffer();
+            query.append("SELECT * FROM item");
+            preparedStatement = connection.prepareStatement(query.toString());
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                String name = rs.getString("item_name");
+                int price = rs.getInt("item_price");
+                int stock = rs.getInt("item_stock");
+                System.out.println(name + " "+ price + " "+ stock);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-//        createItem("돈", 134, 4);
+        for(int i=1; i<=20; i++){
+         createItem("Test"+i, i, i+10);
+        }
+        getItemList();
     }
 }
