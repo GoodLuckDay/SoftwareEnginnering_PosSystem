57a87df186ee7c96bb2654c409b0be3d1adb9933
GoodLuckDay <haon4658@gmail.com>
2017-12-17 04:47:24 +0900

    Resister 등록 관련 클래스 ItemList의 내부 클래스로 이동

--- 4조_PosSystem/src/UIClass/UI_ItemList.java
+++ 4조_PosSystem/src/UIClass/UI_ItemList.java
@@ -81,81 +81,81 @@ public class UI_ItemList extends JFrame {
 		});
 	}
 
-	/*
-	 * for test code
-	 * 현재 클래스에서 실행을 하면 바로 현재 UI가 실행이 가능하게 한다. 
-	 */
-//	public static void main(String[] args){
-//		new UI_ItemList();
-//	}
-}
+	private void ui_Item_Resister(){
+		JTextField text_price;
+		JTextField text_quantity;
+		JTextField text_name;
+	}
 
-class UI_Item_Register extends JFrame {
-	private JTextField text_price;
-	private JTextField text_quantity;
-	private JTextField text_name;
+	class UI_Item_Register extends JFrame {
+		private JTextField text_price;
+		private JTextField text_quantity;
+		private JTextField text_name;
 
-	public UI_Item_Register() {
-		getContentPane().setLayout(null);
+		public UI_Item_Register() {
+			getContentPane().setLayout(null);
 
-		JButton complete = new JButton("\uC644\uB8CC");
-		complete.setFont(new Font("굴림", Font.PLAIN, 24));
-		complete.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		complete.setBounds(393, 30, 190, 63);
-		getContentPane().add(complete);
+			JButton complete = new JButton("\uC644\uB8CC");
+			complete.setFont(new Font("굴림", Font.PLAIN, 24));
+			complete.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			complete.setBounds(393, 30, 190, 63);
+			getContentPane().add(complete);
 
-		JButton cancel = new JButton("\uCDE8\uC18C");
-		cancel.setFont(new Font("굴림", Font.PLAIN, 24));
-		cancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
-		cancel.setBounds(393, 140, 190, 63);
-		getContentPane().add(cancel);
+			JButton cancel = new JButton("\uCDE8\uC18C");
+			cancel.setFont(new Font("굴림", Font.PLAIN, 24));
+			cancel.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+			cancel.setBounds(393, 140, 190, 63);
+			getContentPane().add(cancel);
 
-		JPanel panel = new JPanel();
-		panel.setBounds(14, 12, 330, 215);
-		getContentPane().add(panel);
-		panel.setLayout(new GridLayout(3, 2));
+			JPanel panel = new JPanel();
+			panel.setBounds(14, 12, 330, 215);
+			getContentPane().add(panel);
+			panel.setLayout(new GridLayout(3, 2));
 
-		JLabel item_name = new JLabel("\uC0C1\uD488\uBA85");
-		item_name.setFont(new Font("굴림", Font.PLAIN, 30));
-		item_name.setHorizontalAlignment(JLabel.CENTER);
-		panel.add(item_name);
-		text_name = new JTextField();
-		text_name.setHorizontalAlignment(SwingConstants.CENTER);
-		text_name.setFont(new Font("굴림", Font.PLAIN, 20));
-		text_name.setText("\uC815\uC724\uC218");
-		panel.add(text_name);
-		text_name.setColumns(10);
+			JLabel item_name = new JLabel("\uC0C1\uD488\uBA85");
+			item_name.setFont(new Font("굴림", Font.PLAIN, 30));
+			item_name.setHorizontalAlignment(JLabel.CENTER);
+			panel.add(item_name);
+			text_name = new JTextField();
+			text_name.setHorizontalAlignment(SwingConstants.CENTER);
+			text_name.setFont(new Font("굴림", Font.PLAIN, 20));
+			text_name.setText("\uC815\uC724\uC218");
+			panel.add(text_name);
+			text_name.setColumns(10);
 
-		JLabel item_quantity = new JLabel("\uC7AC \uACE0");
-		item_quantity.setFont(new Font("굴림", Font.PLAIN, 30));
-		item_quantity.setHorizontalAlignment(JLabel.CENTER);
-		panel.add(item_quantity);
-		text_quantity = new JTextField();
-		text_quantity.setHorizontalAlignment(SwingConstants.RIGHT);
-		text_quantity.setFont(new Font("굴림", Font.PLAIN, 20));
-		text_quantity.setText("10");
-		panel.add(text_quantity);
-		text_quantity.setColumns(10);
+			JLabel item_quantity = new JLabel("\uC7AC \uACE0");
+			item_quantity.setFont(new Font("굴림", Font.PLAIN, 30));
+			item_quantity.setHorizontalAlignment(JLabel.CENTER);
+			panel.add(item_quantity);
+			text_quantity = new JTextField();
+			text_quantity.setHorizontalAlignment(SwingConstants.RIGHT);
+			text_quantity.setFont(new Font("굴림", Font.PLAIN, 20));
+			text_quantity.setText("10");
+			panel.add(text_quantity);
+			text_quantity.setColumns(10);
 
-		JLabel item_price = new JLabel("\uAC00 \uACA9");
-		item_price.setFont(new Font("굴림", Font.PLAIN, 30));
-		item_price.setHorizontalAlignment(JLabel.CENTER);
-		panel.add(item_price);
-		text_price = new JTextField();
-		text_price.setHorizontalAlignment(SwingConstants.RIGHT);
-		text_price.setText("39,800\uC6D0");
-		text_price.setFont(new Font("굴림", Font.PLAIN, 20));
-		panel.add(text_price);
-		text_price.setColumns(10);
-		this.setResizable(false);
-		this.setSize(600, 280);
-		this.setVisible(true);
+			JLabel item_price = new JLabel("\uAC00 \uACA9");
+			item_price.setFont(new Font("굴림", Font.PLAIN, 30));
+			item_price.setHorizontalAlignment(JLabel.CENTER);
+			panel.add(item_price);
+			text_price = new JTextField();
+			text_price.setHorizontalAlignment(SwingConstants.RIGHT);
+			text_price.setText("39,800\uC6D0");
+			text_price.setFont(new Font("굴림", Font.PLAIN, 20));
+			panel.add(text_price);
+			text_price.setColumns(10);
+			this.setResizable(false);
+			this.setSize(600, 280);
+			this.setVisible(true);
+		}
 	}
 }
+
+
