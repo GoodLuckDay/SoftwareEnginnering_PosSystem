8c87362bcea2a2d5536730215861cb72d9b69f13
GoodLuckDay <haon4658@gmail.com>
2017-12-17 00:54:58 +0900

    판매한 물품에 내역 출력 함수 구현

--- 4조_PosSystem/src/PosDAOClass/SalesInfo.java
+++ 4조_PosSystem/src/PosDAOClass/SalesInfo.java
@@ -4,6 +4,7 @@ import DBCP.DBConnectionPoolMgr;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SalesInfo {
@@ -46,7 +47,41 @@ public class SalesInfo {
                 e.printStackTrace();
             }
         }
+    }
 
+    public static void getSaledItemList(String requestedTime){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DBConnectionPoolMgr.geteDataSource().getConnection();
+            StringBuffer cQuery = new StringBuffer();
+
+            cQuery.append("SELECT * FROM saledItem WHERE paymentTime = ?");
+            preparedStatement = connection.prepareStatement(cQuery.toString());
+            preparedStatement.setString(1, requestedTime);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while(rs.next()){
+                String name = rs.getString("saledItem_name");
+                int count = rs.getInt("saledItem_count");
+                int price = rs.getInt("saledItem_price");
+                System.out.println(name + " "+ count + " "+ price);
+            }
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        } finally
+
+        {
+            try {
+                closeConnectionAndStmt(connection, preparedStatement);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
     private static void closeConnectionAndStmt(Connection connection, PreparedStatement preparedStatement) throws SQLException {
         if (preparedStatement != null) {
@@ -58,10 +93,11 @@ public class SalesInfo {
     }
 
     public static void main(String[] args) {
-        TestItem[] items = new TestItem[10];
-        for(int i=0; i<items.length; i++){
-            items[i] = new TestItem("오레오"+i, 1200 + i, i);
-        }
-        createSaleInfo("2017/11/11 12:37:44", 3500, items);
+//        TestItem[] items = new TestItem[10];
+//        for(int i=0; i<items.length; i++){
+//            items[i] = new TestItem("오레오"+i, 1200 + i, i);
+//        }
+//        createSaleInfo("2017/11/11 12:37", 3500, items);
+        getSaledItemList("2017/11/11 12:37");
     }
 }
