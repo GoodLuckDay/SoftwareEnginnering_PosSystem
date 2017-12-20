a12212c138186003603f2474cdc6ec487ed3160c
gooLim <limdae07@gmail.com>
2017-12-20 23:16:19 +0900

    다시보내기

--- 4조_PosSystem/src/UIClass/UI_SalesMonitor.java
+++ 4조_PosSystem/src/UIClass/UI_SalesMonitor.java
@@ -6,6 +6,7 @@ import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Vector;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
@@ -13,6 +14,11 @@ import java.awt.Font;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
+
+import PosDAOClass.ItemDAO;
+import PosDAOClass.ItemDTO;
+import PosDAOClass.ItemListDTO;
+
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -24,12 +30,12 @@ import java.awt.GridLayout;
 
 import javax.swing.Box;
 import javax.swing.JTextField;
+import java.awt.Color;
 
 
 public class UI_SalesMonitor extends JFrame {
 	private final JButton exitButton = new JButton("취소");
 	private final JButton registButton = new JButton("등록");
-	private final JButton calculateButton = new JButton("계산");
 	private final Vector<String> userColumn = new Vector<String>();
 	private final JPanel listPanel;
 	private  JScrollPane scrollView;	
@@ -42,8 +48,9 @@ public class UI_SalesMonitor extends JFrame {
 	private JLabel chagedMoneylabel;
 	private JLabel receivedMoneylabel;
 	private JLabel remainedMoneylabel;
-	
 
+	ItemListDTO itemListDTO = null;
+	
 	public UI_SalesMonitor() {
 		setResizable(false);
 		setTitle("판매 화면");
@@ -54,7 +61,7 @@ public class UI_SalesMonitor extends JFrame {
 			}
 		});
 		registButton.setFont(new Font("나눔고딕", Font.BOLD, 20));
-		registButton.setBounds(238, 385, 120, 50);
+		registButton.setBounds(191, 385, 120, 50);
 		getContentPane().add(registButton);
 		
 		exitButton.addActionListener(new ActionListener() {
@@ -68,25 +75,12 @@ public class UI_SalesMonitor extends JFrame {
 		getContentPane().setLayout(null);
 		getContentPane().add(exitButton);
 		
-		calculateButton.addActionListener(new ActionListener() { //계산을 수행하는 버튼
-			public void actionPerformed(ActionEvent arg0) {
-				new UI_Calculate();
-			}
-			
-		});
-		calculateButton.setFont(new Font("나눔고딕", Font.BOLD, 20));
-		calculateButton.setBounds(55, 385, 128, 50);
-		getContentPane().add(calculateButton);
-		
 		
 		userColumn.addElement("물품 번호");
 		userColumn.addElement("물품 이름");
 		userColumn.addElement("물품 수량");
 		userColumn.addElement("물품 가격");
 		model = new DefaultTableModel(userColumn, 0);		
-		
-		
-		
 		
 		jTable = new JTable(model);
 		jTable.setFillsViewportHeight(true);
@@ -107,31 +101,26 @@ public class UI_SalesMonitor extends JFrame {
 		for ( int i = 0; i < tcm.getColumnCount(); i++ ) {
 			tcm.getColumn(i).setCellRenderer(dter);
 		}
-		
-		//임의로 값을 주가해봄
-		userRow = new Vector<String>();
-		userRow.addElement("1");
-		userRow.addElement("2");
-		userRow.addElement("3");
-		userRow.addElement("4");
-		model.addRow(userRow);
-		
-		
-		
 			
 		chargedMoney = new JTextField();
+		chargedMoney.setHorizontalAlignment(SwingConstants.RIGHT);
+		chargedMoney.setText("0");
 		chargedMoney.setBounds(692, 387, 225, 19);
 		getContentPane().add(chargedMoney);
 		chargedMoney.setColumns(10);
 		
 		receivedMoney = new JTextField();
+		receivedMoney.setHorizontalAlignment(SwingConstants.RIGHT);
 		receivedMoney.setColumns(10);
 		receivedMoney.setBounds(692, 404, 225, 19);
+		receivedMoney.setText("0");
 		getContentPane().add(receivedMoney);
 		
 		remainedMoney = new JTextField();
+		remainedMoney.setHorizontalAlignment(SwingConstants.RIGHT);
 		remainedMoney.setColumns(10);
 		remainedMoney.setBounds(692, 422, 225, 19);
+		remainedMoney.setText("0");
 		getContentPane().add(remainedMoney);
 		
 		chagedMoneylabel = new JLabel("총 금액");
@@ -152,21 +141,50 @@ public class UI_SalesMonitor extends JFrame {
 		this.setVisible(true);
 	}
 	
-	public class UI_Calculate extends JFrame{
+	
+	class UI_SalesRegister extends JFrame {
 		private JTextField itemid;
+		private JTextField quantity;
+		private JLabel msg;
+		ItemListDTO itemListDTO;
 		
-		public UI_Calculate() {
+		public UI_SalesRegister() {
+			
 			getContentPane().setLayout(null);
-			this.setSize(580, 200);
-			this.setTitle("계산");
+			this.setSize(780, 200);
+			this.setTitle("판매 물품 등록");
 			JButton okButton = new JButton("확 인");
 			okButton.setFont(new Font("나눔고딕", Font.BOLD, 19));
 			okButton.setBounds(592, 12, 146, 55);
 			getContentPane().add(okButton);	
-			okButton.addActionListener(new ActionListener() {
+			
+			okButton.addActionListener(new ActionListener() {				
 				public void actionPerformed(ActionEvent arg0) {
-					//판매 화면에서 총 금액,받은 금액,거스름 돈 을 기입해주고 현재의 정보를 SaledItemDAO에 등록하여 튜플을 추가.
-					//여기서 추가로 초기화 버튼을 만들어 판매화면의 정보를 초기화 하는 기능을 추가할 것을 추천
+					itemListDTO = new ItemListDTO();
+					ArrayList<ItemDTO> items = itemListDTO.items;				
+					Vector tempVector = new Vector();	  
+					
+					int id = Integer.parseInt(itemid.getText());
+					int count = Integer.parseInt(quantity.getText());		
+					
+					ItemDTO tempItemDTO = items.get(id-1);					
+		            
+					 if( count <= tempItemDTO.getItemStock()) {
+					     tempVector.add(id);
+				            tempVector.add(tempItemDTO.getItemName());				           
+				            tempVector.add(count);
+				            tempVector.add(tempItemDTO.getItemPrice());
+
+				            model.addRow(tempVector);
+				            int price = Integer.parseInt(chargedMoney.getText()) + count*tempItemDTO.getItemPrice();
+				            chargedMoney.setText(price+"");
+				    		
+				            msg.setText("");
+			         } 
+					 else {
+						 msg.setText("현재 보유한 재고량보다 신청한 수량이 많습니다.");
+					 }
+					
 				}
 			});
 			
@@ -180,16 +198,17 @@ public class UI_SalesMonitor extends JFrame {
 				}
 			});
 			
-			JButton btnNewButton = new JButton("확인");
-			btnNewButton.setFont(new Font("나눔고딕", Font.BOLD, 20));
-			btnNewButton.setBounds(374, 39, 146, 68);
-			getContentPane().add(btnNewButton);
-			
-			JLabel ITEMID = new JLabel("받은 금액");
+			JLabel ITEMID = new JLabel("물품 번호");
 			ITEMID.setHorizontalAlignment(SwingConstants.CENTER);
 			ITEMID.setFont(new Font("나눔고딕", Font.BOLD, 19));
 			ITEMID.setBounds(14, 37, 140, 68);
 			getContentPane().add(ITEMID);
+			
+			JLabel QUANTITY = new JLabel("수 량");
+			QUANTITY.setHorizontalAlignment(SwingConstants.CENTER);
+			QUANTITY.setFont(new Font("나눔고딕", Font.BOLD, 19));
+			QUANTITY.setBounds(271, 37, 140, 68);
+			getContentPane().add(QUANTITY);
 			
 			itemid = new JTextField();
 			itemid.setHorizontalAlignment(SwingConstants.CENTER);
@@ -197,8 +216,21 @@ public class UI_SalesMonitor extends JFrame {
 			itemid.setBounds(152, 51, 132, 44);
 			getContentPane().add(itemid);
 			itemid.setColumns(10);
+			
+			quantity = new JTextField();
+			quantity.setHorizontalAlignment(SwingConstants.CENTER);
+			quantity.setFont(new Font("나눔고딕", Font.BOLD, 15));
+			quantity.setColumns(10);
+			quantity.setBounds(395, 51, 132, 44);
+			getContentPane().add(quantity);
+			msg = new JLabel();
+			msg.setBounds(150, 120, 400, 20);
+			msg.setForeground(Color.RED);
+			msg.setFont(new Font("나눔고딕", Font.BOLD, 13));
+			getContentPane().add(msg);
 			this.setVisible(true);
+			
+	
 		}
 	}
-
 }
